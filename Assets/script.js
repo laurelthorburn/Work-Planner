949d48e7466c4b1efc65a396395190e6cefab269
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-23 15:56:42 -0600

    Tutor Support commit

--- Assets/script.js
+++ Assets/script.js
@@ -8,14 +8,16 @@ setInterval(updateClock, 1000);
 
 //Console logging each data-time
 $('[data-time]').each(function() {
-    console.log(typeof($(this).data('time')));
+    console.log(typeof($(this).data('time'))); //number = type of
   }) //data is a number
 
  var currentHour = currentTime.format('h');
- console.log(currentHour); //works
+ console.log(typeof(currentHour)); //works, says it's a string so comparing number to string
 
-if($('[data-time]') == currentHour){
-    console.log(($(this).data('time')));
-    console.log(currentHour);
-   $('.form-control').css('background', 'ff6961');
-};
+// if((document.data("time", 9)) == currentHour){
+//     console.log(($(this).data('time')));
+//     console.log(currentHour);
+//    $('.form-control').css("background-color", "#ff6961"); //confirmed css works
+// };
+
+console.log($( "body" ).data("9"));
