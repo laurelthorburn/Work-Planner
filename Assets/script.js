9f00b0aa31a173b4f3fb680423823b80fbc6e872
Laurel Thorburn <laurelthorburn@gmail.com>
2021-09-23 12:57:55 -0600

    Creating function that compares current time to timeblock

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,21 @@
+//Setting current time as a global function
+var currentTime = moment();
+
 function updateClock() {
-    var currentTime = moment();
     $("#currentDay").text(currentTime.format("dddd, MMMM Do"));
 }
 setInterval(updateClock, 1000);
+
+//Console logging each data-time
+$('[data-time]').each(function() {
+    console.log(typeof($(this).data('time')));
+  }) //data is a number
+
+ var currentHour = currentTime.format('h');
+ console.log(currentHour); //works
+
+if($('[data-time]') == currentHour){
+    console.log(($(this).data('time')));
+    console.log(currentHour);
+   $('.form-control').css('background', 'ff6961');
+};
